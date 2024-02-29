68ec270ae32d7147710c3a00e1528819af8b0475
ZacharyBrenk <160687435+ZacharyBrenk@users.noreply.github.com>
2024-02-29 09:59:17 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024KKweek0",
+      "defaultValue": "2024mndu",
       "required": "true"
     },
     { "name": "Match Level",
