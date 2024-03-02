588f3bbba3e723ed7c8ee4cbe66534b907f2dd54
ZacharyBrenk <160687435+ZacharyBrenk@users.noreply.github.com>
2024-03-01 20:48:06 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -49,6 +49,14 @@ var config_data = `
       },
       "required":"true"
     },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png",
+      "clickRestriction": "one",
+      "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
+      "shape": "circle 5 black red true"
+    }
   ],
   "auton": [
     { "name": "Leave Starting Zone",
