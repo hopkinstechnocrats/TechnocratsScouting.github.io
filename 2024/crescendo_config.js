1411824ec71e026c28209ebaf07dc9683d68f703
ZacharyBrenk <160687435+ZacharyBrenk@users.noreply.github.com>
2024-08-13 20:01:31 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "ScoutingPass2024",
-  "page_title": "testing",
+    "page_title": "TESTING",
   "checkboxAs": "10",
   "prematch": [
     { "name": "scouter Initials",
