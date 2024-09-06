0f63dfdfca31aa52db144dee0fa2855b27c2536e
ZacharyBrenk <zachbrenk@gmail.com>
2024-09-05 20:27:17 -0500

    Added my own API key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "mwHlGJssfKM7Nnr5XsrJrSdXzrISwT65pagpf7yyfWiOHLn982G1tTuvwUTMVaCW";
 /**
  * Get list of teams in event
  *
